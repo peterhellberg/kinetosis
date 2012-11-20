ca4e7f5d0406c455331d8d22b5286de33ea7ae40
Peter Hellberg <peter@c7.se>
2012-11-20 01:58:40 +0100

    Demo script that will (hopefully) run on machines with a Sudden Motion Sensor.

--- demo.rb
+++ demo.rb
@@ -1,5 +1,5 @@
 # Load in the extension (on OS X this loads ./MyTest/mytest.bundle - unsure about Linux, possibly mytest.so)
-require File.dirname(__FILE__) + '/lib/kinetosis'
+require './lib/kinetosis'
 
 # Kinetosis is now a module, so we need to include it
 include Kinetosis
@@ -7,8 +7,13 @@ include Kinetosis
 # Trap ^C and exit
 trap("SIGINT") { exit! }
 
-# Call and print the result from the xyz method
-loop do
-  c = xyz.map { |v| v.to_s.ljust(4) }
-  puts "x: #{c[0]} y: #{c[1]} z: #{c[2]}"
+unless has_sudden_motion_sensor?
+  puts "ERROR: Are you sure that your machine has a Sudden Motion Sensor?"
+else
+  # Call and print the result from the xyz method
+  loop do
+    c = xyz.map { |v| v.to_s.ljust(4) }
+    puts "x: #{c[0]} y: #{c[1]} z: #{c[2]}"
+  end
 end
+
